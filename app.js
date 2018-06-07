0e2ec3baa18b92c6e17d7074ec98ff79bc07e64f
Lee Brandt <leebrandt@gmail.com>
2018-06-07 10:16:08 -0500

    removed unused import

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import createError from "http-errors";
 import express, { json, urlencoded } from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 const mongoose = require('mongoose');
@@ -28,12 +27,12 @@ app.use('/', indexRouter);
 app.use('/speakers', speakersRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
